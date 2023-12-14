33b16ad6caff59429483f1ca132c4ed63b97c185
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-14 13:54:51 +0700

    chore : fix path to authRoutes.js

--- app/routes/moviesRoutes.js
+++ app/routes/moviesRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const moviesController = require("../controllers/moviesController");
-const { authorize } = require("./auth");
+const { authorize } = require("./authRoutes");
 
 /**
  * @swagger
