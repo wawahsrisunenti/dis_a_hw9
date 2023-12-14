1b47275283eaecf636ff438e9bcbf5e22e89c928
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-14 15:22:48 +0700

    refactor: update authModel.js to use a common database connection pool and improve user querie

--- app/models/authModels.js
+++ app/models/authModels.js
@@ -1,62 +1,18 @@
-const pool = require("../../config/dis_queries");
-const jwt = require("jsonwebtoken");
+const pool = require("../../config/queries");
 
-function loginUser(email, password, callback) {
+const getUserByEmail = (email, callback) => {
+  pool.query("SELECT * FROM users WHERE email = $1", [email], callback);
+};
+
+const createUser = (email, password, gender, role, callback) => {
   pool.query(
-    "SELECT * FROM users WHERE email = $1",
-    [email],
-    (error, results) => {
-      if (error) {
-        callback(error, null);
-        return;
-      }
-
-      if (results.rows.length === 1) {
-        const user = results.rows[0];
-        if (user.password === password) {
-          const token = jwt.sign(
-            { email: email, id: user.id },
-            "jumintenParkinson",
-            { expiresIn: "1h" }
-          );
-          callback(null, token);
-        } else {
-          callback({ message: "Gagal login" }, null);
-        }
-      } else {
-        callback({ message: "Gagal login" }, null);
-      }
-    }
+    "INSERT INTO users (email, password, gender, role) VALUES ($1, $2, $3, $4)",
+    [email, password, gender, role],
+    callback
   );
-}
+};
 
-function registerUser(email, password, gender, role, callback) {
-  pool.query("SELECT MAX(id) AS max_id FROM users", (error, results) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-
-    const maxId = results.rows[0].max_id || 0;
-    const newId = maxId + 1;
-
-    pool.query(
-      "INSERT INTO users (id, email, password, gender, role) VALUES ($1, $2, $3, $4, $5)",
-      [newId, email, password, gender, role],
-      (error, results) => {
-        if (error) {
-          callback(error, null);
-          return;
-        }
-
-        const user = {
-          email: email,
-          id: newId,
-        };
-        callback(null, user);
-      }
-    );
-  });
-}
-
-module.exports = { loginUser, registerUser };
+module.exports = {
+  getUserByEmail,
+  createUser,
+};
