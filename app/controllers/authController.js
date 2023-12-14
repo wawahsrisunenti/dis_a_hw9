0651eccaab20893e6c74db9535a4b42cf4d30bca
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-14 12:42:09 +0700

    feat: implementation of authController.js

---
+++ app/controllers/authController.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const pool = require("../../config/dis_queries");
+const { TokenExpiredError } = require("jsonwebtoken");
+
+function authorize(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token)
+    return res
+      .status(401)
+      .json({
+        message:
+          "The door is closed! No token, no entry. Access denied. Tokens are not provided.",
+      });
+
+  try {
+    const decoded = jwt.verify(token, "jumintenParkinson");
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    if (ex instanceof TokenExpiredError) {
+      res.status(401).json({ message: "The jig is up! Token has expired" });
+    } else {
+      res.status(400).json({ message: "Thats a bum steer! Invalid Token" });
+    }
+  }
+}
+
+module.exports = { authorize };
