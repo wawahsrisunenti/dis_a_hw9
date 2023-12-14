8dfe018f925dd233c1b7c4f56a819b906ae66f4e
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-14 10:47:46 +0700

    chore: resolve identifier problem in the registration process within auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -46,53 +46,46 @@ router.post("/register", (req, res) => {
   const { email, password, gender, role } = req.body;
 
   // Cek apakah email sudah terdaftar
-  pool.query(
-    "SELECT * FROM users WHERE email = $1",
-    [email],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-
-      if (results.rows.length > 0) {
-        return res.status(400).json({ message: "Email sudah terdaftar" });
-      }
-
-      // Jika email belum terdaftar, tambahkan pengguna baru
-      pool.query(
-        "INSERT INTO users (email, password, gender, role) VALUES ($1, $2, $3, $4) RETURNING id",
-        [email, password, gender, role],
-        (error, results) => {
-          if (error) {
-            throw error;
-          }
-
-          // Buat token otentikasi untuk pengguna yang baru mendaftar
-          const user = {
-            email: email,
-            id: results.rows[0].id,
-          };
-          const token = jwt.sign(user, "jumintenParkinson");
-          res.json({
-            message: "Its a piece of cake! Register Sucessfully",
-            token,
-          });
-        }
-      );
+  pool.query("SELECT MAX(id) AS max_id FROM users", (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+
+    const maxId = results.rows[0].max_id || 0;
+    const newId = maxId + 1;
+
+    // Jika email belum terdaftar, tambahkan pengguna baru (id baru)
+    pool.query(
+      'INSERT INTO users (id, email, password, gender, role) VALUES ($1, $2, $3, $4, $5)',
+      [newId, email, password, gender, role],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+
+        // Buat token otentikasi untuk pengguna yang baru mendaftar
+        const user = {
+          email: email,
+          id: newId
+        };
+        const token = jwt.sign(user, "jumintenParkinson");
+        res.json({
+          message: "Its a piece of cake! Register Sucessfully",
+          token,
+        });
+      }
+    );
+  });
 });
 
 function authorize(req, res, next) {
   // Periksa token otentikasi
   const token = req.header("x-auth-token");
   if (!token)
-    return res
-      .status(401)
-      .json({
-        message:
-          "The door is closed! No token, no entry. Access denied. Tokens are not provided.",
-      });
+    return res.status(401).json({
+      message:
+        "The door is closed! No token, no entry. Access denied. Tokens are not provided.",
+    });
 
   try {
     const decoded = jwt.verify(token, "jumintenParkinson");
