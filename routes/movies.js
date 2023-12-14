bbdd730f72416aa141dc97aab330f4ae89611ccc
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-14 10:36:54 +0700

    feat(movies.js): implement CRUD operations for movies
    Enhance the movies.js file to include CRUD operations (Create, Read, Update, Delete) for movie data. Ensure that authentication is required for these operations to improve security.

--- routes/movies.js
+++ routes/movies.js
@@ -1,151 +1,22 @@
-import express from "express";
-import pool from "../dis_queries.js";
-import { authorize } from "./auth.js";
-
+const express = require("express");
 const router = express.Router();
+const pool = require("../dis_queries");
+const { authorize } = require("./auth");
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Movie:
- *       type: object
- *       properties:
- *         id:
- *           type: integer
- *         title:
- *           type: string
- *         genres:
- *           type: string
- *         year:
- *           type: integer
- *
- *     MovieInput:
- *       type: object
- *       properties:
- *         title:
- *           type: string
- *         genres:
- *           type: string
- *         year:
- *           type: integer
- *
- * /movies:
- *   get:
- *     summary: Get all movies
- *     description: Retrieve a list of all movies.
- *     parameters:
- *       - in: query
- *         name: page
- *         schema:
- *           type: integer
- *         description: The page number (default is 1).
- *       - in: query
- *         name: limit
- *         schema:
- *           type: integer
- *         description: The number of items per page (default is 10).
- *     responses:
- *       200:
- *         description: A list of movies.
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Movie'
- *
- *   post:
- *     summary: Add a new movie
- *     description: Add a new movie to the list.
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/MovieInput'
- *     responses:
- *       200:
- *         description: Movie added successfully.
- *
- * /movies/{id}:
- *   get:
- *     summary: Get a movie by ID
- *     description: Retrieve a movie by its ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: Movie ID
- *     responses:
- *       200:
- *         description: A movie.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Movie'
- *
- *   put:
- *     summary: Update a movie by ID
- *     description: Update a movie's details by its ID.
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: Movie ID
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/MovieInput'
- *     responses:
- *       200:
- *         description: Movie updated successfully.
- *
- *   delete:
- *     summary: Delete a movie by ID
- *     description: Delete a movie by its ID.
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: Movie ID
- *     responses:
- *       200:
- *         description: Movie deleted successfully.
- */
+// Middleware otentikasi untuk seluruh rute dalam file movies.js (memerlukan otentikasi)
+router.use(authorize);
 
-router.use(authorize); // Middleware for otentikasi
-
+// GET semua data movies
 router.get("/", (req, res) => {
-  const page = req.query.page || 1;
-  const limit = req.query.limit || 10;
-  const offset = (page - 1) * limit;
-  pool.query(
-    "SELECT * FROM movies OFFSET $1 LIMIT $2",
-    [offset, limit],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      res.json(results.rows);
+  pool.query("SELECT * FROM movies", (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    res.json(results.rows);
+  });
 });
 
+// GET data movie berdasarkan ID
 router.get("/:id", (req, res) => {
   const movieId = req.params.id;
   pool.query(
@@ -160,13 +31,20 @@ router.get("/:id", (req, res) => {
   );
 });
 
+// POST data movie baru (memerlukan otentikasi)
 router.post("/", (req, res) => {
   const { title, genres, year } = req.body;
+
+  // Ambil ID terakhir dari tabel
   pool.query("SELECT MAX(id) FROM movies", (error, results) => {
     if (error) {
       throw error;
     }
+
+    // Hitung ID baru
     const newId = results.rows[0].max + 1;
+
+    // Masukkan data baru dengan ID baru
     pool.query(
       "INSERT INTO movies (id, title, genres, year) VALUES ($1, $2, $3, $4)",
       [newId, title, genres, year],
@@ -183,6 +61,7 @@ router.post("/", (req, res) => {
   });
 });
 
+// PUT (update) data movie berdasarkan ID (memerlukan otentikasi)
 router.put("/:id", (req, res) => {
   const movieId = req.params.id;
   const { title, genres, year } = req.body;
@@ -200,6 +79,7 @@ router.put("/:id", (req, res) => {
   );
 });
 
+// DELETE data movie berdasarkan ID (memerlukan otentikasi)
 router.delete("/:id", (req, res) => {
   const movieId = req.params.id;
   pool.query(
@@ -216,4 +96,4 @@ router.delete("/:id", (req, res) => {
   );
 });
 
-export default router;
+module.exports = router;
