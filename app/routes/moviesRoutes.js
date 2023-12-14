881819ae4493b373d181bce5d3f5247ac94c3196
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-14 14:34:30 +0700

    chore: perform a forceful restart

--- app/routes/moviesRoutes.js
+++ app/routes/moviesRoutes.js
@@ -1,17 +1,217 @@
 const express = require("express");
 const router = express.Router();
-const moviesController = require("../controllers/moviesController");
-const { authorize } = require("./authRoutes");
+const pool = require("../dis_queries");
+const { authorize } = require("./auth");
 
 /**
  * @swagger
- * ... (Dokumentasi Swagger untuk rute movies)
+ * components:
+ *   schemas:
+ *     Movie:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         title:
+ *           type: string
+ *         genres:
+ *           type: string
+ *         year:
+ *           type: integer
+ *
+ *     MovieInput:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *         genres:
+ *           type: string
+ *         year:
+ *           type: integer
+ *
+ * /movies:
+ *   get:
+ *     summary: Get all movies
+ *     description: Retrieve a list of all movies.
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: The page number (default is 1).
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: The number of items per page (default is 10).
+ *     responses:
+ *       200:
+ *         description: A list of movies.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Movie'
+ *
+ *   post:
+ *     summary: Add a new movie
+ *     description: Add a new movie to the list.
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MovieInput'
+ *     responses:
+ *       200:
+ *         description: Movie added successfully.
+ *
+ * /movies/{id}:
+ *   get:
+ *     summary: Get a movie by ID
+ *     description: Retrieve a movie by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Movie ID
+ *     responses:
+ *       200:
+ *         description: A movie.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Movie'
+ *
+ *   put:
+ *     summary: Update a movie by ID
+ *     description: Update a movie's details by its ID.
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Movie ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MovieInput'
+ *     responses:
+ *       200:
+ *         description: Movie data updated successfully
+ *
+ *   delete:
+ *     summary: Delete a movie by ID
+ *     description: Delete a movie by its ID.
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Movie ID
+ *     responses:
+ *       200:
+ *         description: Movie data has been successfully deleted
  */
+router.use(authorize); // Middleware untuk otentikasi
 
-router.get("/", moviesController.getMovies);
-router.get("/:id", moviesController.getMovieById);
-router.post("/", authorize, moviesController.addMovie);
-router.put("/:id", authorize, moviesController.updateMovie);
-router.delete("/:id", authorize, moviesController.deleteMovie);
+router.get("/", (req, res) => {
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 10;
+  const offset = (page - 1) * limit;
+  pool.query(
+    "SELECT * FROM movies OFFSET $1 LIMIT $2",
+    [offset, limit],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.json(results.rows);
+    }
+  );
+});
+
+router.get("/:id", (req, res) => {
+  const movieId = req.params.id;
+  pool.query(
+    "SELECT * FROM movies WHERE id = $1",
+    [movieId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.json(results.rows[0]);
+    }
+  );
+});
+
+router.post("/", (req, res) => {
+  const { title, genres, year } = req.body;
+  pool.query("SELECT MAX(id) FROM movies", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    const newId = results.rows[0].max + 1;
+    pool.query(
+      "INSERT INTO movies (id, title, genres, year) VALUES ($1, $2, $3, $4)",
+      [newId, title, genres, year],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.json({
+          message:
+            "New movie data has been successfully added with ID " + newId,
+        });
+      }
+    );
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const movieId = req.params.id;
+  const { title, genres, year } = req.body;
+  pool.query(
+    "UPDATE movies SET title = $1, genres = $2, year = $3 WHERE id = $4",
+    [title, genres, year, movieId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.json({
+        message: "Movie data updated successfully",
+      });
+    }
+  );
+});
+
+router.delete("/:id", (req, res) => {
+  const movieId = req.params.id;
+  pool.query(
+    "DELETE FROM movies WHERE id = $1",
+    [movieId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.json({
+        message: "Movie data has been successfully deleted",
+      });
+    }
+  );
+});
 
 module.exports = router;
